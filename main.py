96e10fca37c2154e14748d7a2cf7bc980351da5d
Esa Varemo <esa@kuivanto.fi>
2018-09-19 01:09:59 +0300

    Initial implementation finished

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import datetime
 import os
-import json
+import sys
 
-from flask import Flask, redirect, render_template, request
+from flask import Flask, request
 
 # Initialize app
 
@@ -14,15 +14,46 @@ else:
     token = os.environ['TOKEN']
     authenticate = True
 
+if 'CONFIG_PATH' not in os.environ.keys():
+    print('You need to pass in the path to store config files in as environment variable "CONFIG_PATH"')
+    sys.exit(1)
+path = os.environ['CONFIG_PATH']
+
+
+template = """[
+    {{
+        "targets": ["{}"]
+    }}
+]"""
+
+
+def get_config(hostname):
+    return template.format(hostname)
+
+
 @app.route('/')
 def root():
     return 'Please POST {"hostname": "..." [, "token": "..."]} to /register'
 
+
 @app.route('/register', methods=['POST'])
 def register():
-    host = request.json('hostname')
-    print(host)
-    return "OK"
+    if authenticate:
+        if request.json['token'] != token:
+            return "Authentication error"
+
+    host = request.json['hostname']
+    file = os.path.join(path, host)
+
+    exists = os.path.isfile(file)
+
+    with open(os.path.join(path, host), 'w') as hostfile:
+        hostfile.write(get_config(host))
+
+    if exists:
+        return "OK"
+    else:
+        return "Created"
 
 
 # Basic healthcheck for orchestration and monitoring
